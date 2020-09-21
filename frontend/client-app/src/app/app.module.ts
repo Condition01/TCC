b83b62bbc77c82f8260a06b99a5f2d192b361bdf
Bruno <bruno.cardoso>
2020-09-20 22:49:28 -0300

    adicionado primeiras telas de template

--- frontend/client-app/src/app/app.module.ts
+++ frontend/client-app/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { TopMenuComponent } from './components/template/top-menu/top-menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatRippleModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
