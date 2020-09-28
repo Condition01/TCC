f36a352d9352a57ded426e308d5b284c73604992
Bruno <bruno.cardoso>
2020-09-27 22:50:04 -0300

    adicionado testapp

---
+++ app-services/testapp/src/main/java/br/com/ifeira/auth/TestappApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifeira.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestappApplication.class, args);
+	}
+
+}
