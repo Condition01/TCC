36c87345340abb31e3675244240160dbf3e8fdfa
Bruno <bruno.cardoso>
2020-10-12 01:01:33 -0300

    ajustado a autenticação, headers para funcionamento adicionadas no zuul proxy

--- app-services/auth/src/main/java/br/com/ifeira/auth/AuthApplication.java
+++ app-services/auth/src/main/java/br/com/ifeira/auth/AuthApplication.java
@@ -2,11 +2,13 @@ package br.com.ifeira.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+@EnableEurekaClient
 @EnableResourceServer
 @EnableAuthorizationServer
 @SpringBootApplication
