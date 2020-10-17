34bce45a57f03a8d230f753d1fc1e15f802cd9dc
Bruno <bruno.cardoso>
2020-10-17 16:46:20 -0300

    removido estilo dos cards

--- frontend/client-app/src/app/views/login/login.component.ts
+++ frontend/client-app/src/app/views/login/login.component.ts
@@ -8,10 +8,9 @@ import { NotificacaoService } from 'src/app/services/notificacao.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   bannerSubject: Subject<any> = new Subject<any>();
 
   constructor(
@@ -20,31 +19,42 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private formBuilder: FormBuilder,
     private notificacaoService: NotificacaoService
-    ) { }
+  ) {}
 
-  form: FormGroup
+  form: FormGroup;
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       login: [null, [Validators.required]],
-      senha: [null, [Validators.required]]
-    })
+      senha: [null, [Validators.required]],
+    });
   }
 
   logar() {
     let login = this.form.get('login').value;
     let senha = this.form.get('senha').value;
-    if(login && senha) {
-      this.authService.login(login, senha).subscribe(login => {
-        this.authService.getUsuario().subscribe(user => {
-          this.notificacaoService.sucessNotification('Logado com sucesso!');
-          this.router.navigate(['/']);
-        })    
-      })
-    }else{
-      this.notificacaoService.normalNotification('É necessario digitar o Usuario e Senha');
+    if (login && senha) {
+      this.authService.login(login, senha).subscribe((login) => {
+        this.authService.getUsuario().subscribe(
+          (user) => {
+            this.notificacaoService.sucessNotification('Logado com sucesso!');
+            this.router.navigate(['/']);
+          }
+        );
+      },
+      (err) => {
+        if(err.error.error_description.includes('inexistente ou senha')){
+          this.notificacaoService.normalNotification('Usuario ou Senha Inválidos');
+        }else{
+          this.notificacaoService.normalNotification('erro desconhecido!!');
+        }
+
+      });
+    } else {
+      this.notificacaoService.normalNotification(
+        'É necessario digitar o Usuario e Senha'
+      );
     }
     // this.notificacaoService.errorNotification('Cadastro Efetuado!')
   }
-
 }
