2dd90868748c7076c8aa18cecfccc6a576ffc9b1
bruno cardoso <ext.thiago.fukushima@aviva.com.br>
2021-04-20 23:44:03 -0300

    referencia pra controladora na consumidora

--- app-services/pagamento/src/main/java/br/com/ifeira/pagamento/boundary/PagamentoConsumer.java
+++ app-services/pagamento/src/main/java/br/com/ifeira/pagamento/boundary/PagamentoConsumer.java
@@ -1,9 +1,11 @@
 package br.com.ifeira.pagamento.boundary;
 
+import br.com.ifeira.pagamento.controller.PagamentoController;
 import br.com.ifeira.pagamento.shared.dto.PagamentoDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,10 +13,12 @@ public class PagamentoConsumer {
 
     private Logger logger = LoggerFactory.getLogger(PagamentoConsumer.class);
 
+    @Autowired
+    private PagamentoController pagamentoController;
+
     @RabbitListener(queues = "${mq[0].queue-name}")
     public void consumir(PagamentoDTO pagamento) {
-
-
+        this.pagamentoController.processarRequisicao(pagamento);
     }
 
 }
