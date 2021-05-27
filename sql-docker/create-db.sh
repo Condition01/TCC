968f69db634615ba752c4c568d72e8dd73b350c4
bruno cardoso <ext.thiago.fukushima@aviva.com.br>
2021-05-27 01:28:21 -0300

    ajustes finais

--- sql-docker/create-db.sh
+++ sql-docker/create-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name mysqltcc -p 3306:3306 -v ~/mysqltcc:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_ROOT_HOST="%" mysql:8.0
+docker run -d --name mysqltcc -p 3306:3306 -v ~/mysqltcc:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_ROOT_HOST="%" -e MYSQL_DATABASE="IFEIRA" mysql:8.0
